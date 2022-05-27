44eab20d30deb52d5075e30e6c5e6336520e1c37
JohnFernandes <mac@MacBook-Air-de-mac.local>
2022-05-27 10:05:24 -0300

    Projeto Criado e Comment Component Criado

---
+++ src/components/Comment.jsx
@@ -0,0 +1,10 @@
+import './Comment.css';
+
+export function Comment(){
+    return (
+        <div className="comment">
+            <h2>Jonathan Costa</h2>
+            <p>o Jonatha é Lindo</p>
+        </div>
+    )
+}

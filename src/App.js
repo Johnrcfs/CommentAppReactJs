8435bf44d70a88809b573e71d27d4d7ca8635e26
Johnrcfs <j.fernandes-costa@outlook.com>
2022-05-27 14:12:57 -0300

    Adicionando um formulario e crianos um novo state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
+import React from 'react';
 import './App.css';
+import {Comment} from './components/Comment';
 
-import { Comment } from './components/Comment';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My Comments App</h1>
-      <Comment
-        name='Jonathan Costa'
-        email='johnrsfc.ti@gmail.com'
-        date={new Date()}
-        message='o John é superLindo'
-      />
-      <Comment
-        name='Larissa Silva'
-        email='laarirebeca3@gmail.com'
-        date={new Date()}
-        message='a Lary é SUPER LINDA'
-      />
-      
-    </div>
-  );
-}
+  state = {
+    comments: [
+      {
+        name: 'Jonathan',
+        email: 'John@John.com',
+        date: new Date(),
+        message: 'John é lindo demais',
+      },
+      {
+        name: 'larissa',
+        email: 'lari@John.com',
+        date: new Date(),
+        message: 'John é lindo demais',
+      },
+    ],
+    form: {
+      name: '',
+      email: '',
+      message: '',
+    }
+  };
+
+  addComment = (event) => {
+    event.preventDefault();
+    const newComment = {...this.state.form, date: new Date()};
+
+    this.setState({
+      comments: [...this.state.comments, newComment],
+      form: {
+        name: '',
+        email: '',
+        message: '',
+      }
+    });
+
+  };
+
+  //capturando todos o evento digitado pelo usuario.
+  onFieldChanges = (event) => {
+    const newCommentForm = this.state.form;
+    newCommentForm[event.target.name] = event.target.value;
+
+    this.setState({
+      form: newCommentForm
+    })
+
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>My Comments Apps</h1>
+
+        {this.state.comments.map((comment, index) => {
+          return (
+            <Comment
+              key={index}
+              name={comment.name}
+              email={comment.email}
+              date={comment.date}
+              message={comment.message}
+
+            />
+          );
+        })};
+        <form onSubmit={this.addComment}>
+          <h2>Add Comment</h2>
+
+          <div>
+            <input
+              onChange={this.onFieldChanges}
+              value={this.state.form.name}
+              type='text'
+              name='name'
+              placeholder='Type here your Name'
+              required='required'
+            />
+          </div>
+
+          <div>
+            <input
+              onChange={this.onFieldChanges}
+              value={this.state.form.email}
+              type='email'
+              name='email'
+              placeholder='Type here your Email'
+              required='required'
+            />
+          </div>
+
+          <div>
+            <textarea 
+            onChange={this.onFieldChanges}
+            value={this.state.form.message}
+            name='message' 
+            rows='4' 
+            placeholder='Type your Message'
+            required='required'
+            />
+          </div>
+
+          <button type='submit'>Add a Comment</button>
+        </form>
+      </div>
+    );
+  };
+
+};
+
+
+
 
 export default App;
